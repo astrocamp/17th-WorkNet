e09f76f0d534b13f09e7220aec0cc0ade5b9dee5
Ma <z2715674@gmail.com>
2024-08-28 18:42:08 +0800

    refactor: core rename config

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("users.urls")),
 ] + debug_toolbar_urls()
