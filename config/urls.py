60f02e05c80353251fad95a120dae28815172f88
Ma <z2715674@gmail.com>
2024-08-28 20:40:01 +0800

    refactor: users change apps.users

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,5 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("users.urls")),
+    path("", include("apps.users.urls")),
 ] + debug_toolbar_urls()
