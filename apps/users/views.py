1c00a4a6b3cedbcbe7103bc613bfeafc874263dc
Ma <z2715674@gmail.com>
2024-08-29 15:16:16 +0800

    feat: add regiterAutoLogin & req rename request

--- apps/users/views.py
+++ apps/users/views.py
@@ -6,54 +6,57 @@ from apps.users.models import User
 from .forms import CustomUserCreationForm
 
 
-def home(req):
-    return render(req, "users/home.html")
+def home(request):
+    return render(request, "users/home.html")
 
+def index(request):
+    return render(request, "users/home.html")
+    
 
-def index(req):
-    if req.method == "POST":
-        form = CustomUserCreationForm(req.POST)
+def register(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+
         if form.is_valid():
-            form.save()
-            messages.success(req, "註冊成功")
-            return redirect("users:index")
+            user = form.save()  
+            login(request, user) 
+            messages.success(request, "註冊成功，已自動登入")
+            return redirect("users:index")  
         else:
-            return render(req, "users/register.html", {"form": form})
-    return render(req, "users/home.html")
+            return render(request, "users/register.html", {"form": form})
 
-
-def register(req):
+def register(request):
     form = CustomUserCreationForm()
-    return render(req, "users/register.html", {"form": form})
+    return render(request, "users/register.html", {"form": form})
 
 
-def sign_in(req):
-    if req.method == "POST":
-        username = req.POST.get("username")
-        password = req.POST.get("password")
-        next_url = req.POST.get("next")
+def sign_in(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        next_url = request.POST.get("next")
 
         user = authenticate(username=username, password=password)
 
         if user is not None:
-            login(req, user)
+            login(request, user)
 
-            messages.success(req, "登入成功")
+            messages.success(request, "登入成功")
             if next_url:
                 return redirect(next_url)
 
             return redirect("users:index")
         else:
-            messages.error(req, "登入失敗")
+            messages.error(request, "登入失敗")
             return redirect("users:sign_in")
 
-    return render(req, "users/sign_in.html")
+    return render(request, "users/sign_in.html")
 
 
-def sign_out(req):
-    if req.method == "POST":
-        logout(req)
-        messages.success(req, "登出成功")
+def sign_out(request):
+    if request.method == "POST":
+        logout(request)
+        messages.success(request, "登出成功")
         return redirect("users:index")
 
 
