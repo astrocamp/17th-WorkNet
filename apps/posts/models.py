dbbb343663426ba490453f36f97333de7920553a
ellen <wding975@gmail.com>
2024-09-03 02:03:20 +0800

    feat: add posts like/dislike

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -4,6 +4,8 @@ from apps.companies.models import Company
 from apps.users.models import User
 from lib.models.soft_delete import SoftDeleteManager
 
+from ..users.models import User
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -11,6 +13,9 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(default=None, null=True)
+    user = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True)
+    like_cnt = models.IntegerField(default=0)
+    dislike_cnt = models.IntegerField(default=0)
 
     user = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True)
     company = models.ForeignKey(to=Company, on_delete=models.CASCADE, null=True)
@@ -32,3 +37,10 @@ class Comment(models.Model):
     deleted_at = models.DateTimeField(default=None, null=True)
 
     objects = SoftDeleteManager()
+
+
+class LikeLog(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    post = models.ForeignKey(to=Post, on_delete=models.CASCADE)
+    like_type = models.IntegerField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now=True)
