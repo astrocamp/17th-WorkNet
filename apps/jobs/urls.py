bbda67c91006c31e5b00d9e9e21a71efe326ac89
Gujiahan <gujiahan817@gmail.com>
2024-09-03 00:15:28 +0800

    feat: add job favorite
    BREAKING CHANGE:
    
    fix: delete a profile.id
    
    BREAKING CHANGE:

--- apps/jobs/urls.py
+++ apps/jobs/urls.py
@@ -7,7 +7,14 @@ app_name = "jobs"
 urlpatterns = [
     path("", views.index, name="index"),
     path("new", views.new, name="new"),
+    path("favorites/", views.favorites_list, name="favorites_list"),
+    path("<int:id>/favorite", views.favorite, name="favorite"),
     path("<int:id>", views.show, name="show"),
     path("<int:id>/edit", views.edit, name="edit"),
     path("<int:id>/delete", views.delete, name="delete"),
+    path(
+        "favorites/<int:id>/favorites_delete",
+        views.favorites_delete,
+        name="favorites_delete",
+    ),
 ]
