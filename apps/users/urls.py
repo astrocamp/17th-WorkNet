756904a5a25cf659e1ea75e418424924a9526ee8
Ma <z2715674@gmail.com>
2024-09-03 15:49:01 +0800

    feat: add password_reset send mail ＆mailgun's sandbox test

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 from .views import PasswordResetDoneView, PasswordResetView
@@ -11,13 +12,16 @@ urlpatterns = [
     path("sign_out/", views.sign_out, name="sign_out"),
     path("register/", views.register, name="register"),
     path("<int:id>/info/", views.info, name="info"),
+
     path("login_redirect/", views.login_redirect, name="login_redirect"),
     path("password_reset/", PasswordResetView.as_view(), name="password_reset"),
     path(
+
         "password_reset_done/",
         PasswordResetDoneView.as_view(),
         name="password_reset_done",
     ),
+
     path("favorites/", views.favorites_list, name="favorites_list"),
     path("<int:id>/favorite", views.favorite, name="favorite"),
     path(
@@ -25,4 +29,5 @@ urlpatterns = [
         views.favorites_delete,
         name="favorites_delete",
     ),
+
 ]
