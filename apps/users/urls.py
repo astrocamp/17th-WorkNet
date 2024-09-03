47b34e5bde42d0d79607a0b27207a9afbd895ff0
Kk <iankk0806@gmail.com>
2024-09-03 17:16:18 +0800

    refactor: let google login can redirect Userinfo

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("sign_in/", views.sign_in, name="sign_in"),
     path("sign_out/", views.sign_out, name="sign_out"),
     path("register/", views.register, name="register"),
-    path("<int:id>/info/", views.info, name="info"),
+    path("<int:id>/info/", views.info, name="info"),  # 第三方登入轉址
+    path("login-redirect/", views.login_redirect, name="login_redirect"),
 ]
