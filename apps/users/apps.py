91e859867025bc27085c0184e1c146fde7fe6470
Kk <iankk0806@gmail.com>
2024-09-04 13:04:20 +0800

    refactor: indent & add .prettierignore file

--- apps/users/apps.py
+++ apps/users/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.users"
-
-    def ready(self):
-        import apps.users.views  # 確保信號被導入
