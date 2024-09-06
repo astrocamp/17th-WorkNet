be583b3fd15361c8371442417778e2a31be5f503
Ma <z2715674@gmail.com>
2024-09-06 17:39:53 +0800

    refactor: resolve conflicts

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -12,18 +12,13 @@ urlpatterns = [
     path("sign_out/", views.sign_out, name="sign_out"),
     path("register/", views.register, name="register"),
     path("<int:id>/info/", views.info, name="info"),
-
     path("login-redirect/", views.login_redirect, name="login_redirect"),
-
     path("password_reset/", PasswordResetView.as_view(), name="password_reset"),
     path(
-
         "password_reset_done/",
         PasswordResetDoneView.as_view(),
         name="password_reset_done",
     ),
-
-
     path("favorites/", views.favorites_list, name="favorites_list"),
     path("<int:id>/favorite", views.favorite, name="favorite"),
     path(
@@ -31,6 +26,4 @@ urlpatterns = [
         views.favorites_delete,
         name="favorites_delete",
     ),
-
-
 ]
