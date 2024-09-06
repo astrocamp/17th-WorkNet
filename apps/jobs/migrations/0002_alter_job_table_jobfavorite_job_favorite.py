6e4c41e84e8daac21a54cfee33bdf088befed996
Gujiahan <gujiahan817@gmail.com>
2024-09-06 13:36:40 +0800

    fix: add redirect to previous page after login
    BREAKING CHANGE:

--- apps/jobs/migrations/0002_alter_job_table_jobfavorite_job_favorite.py
+++ apps/jobs/migrations/0002_alter_job_table_jobfavorite_job_favorite.py
@@ -8,31 +8,55 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('jobs', '0001_initial'),
+        ("jobs", "0001_initial"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.AlterModelTable(
-            name='job',
-            table='company_job',
+            name="job",
+            table="company_job",
         ),
         migrations.CreateModel(
-            name='JobFavorite',
+            name="JobFavorite",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('favorited_at', models.DateTimeField(auto_now_add=True)),
-                ('job', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='favorited_by', to='jobs.job')),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='user_favorite', to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("favorited_at", models.DateTimeField(auto_now_add=True)),
+                (
+                    "job",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="favorited_by",
+                        to="jobs.job",
+                    ),
+                ),
+                (
+                    "user",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="user_favorite",
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
             options={
-                'db_table': 'favorite_job',
-                'unique_together': {('user', 'job')},
+                "db_table": "favorite_job",
+                "unique_together": {("user", "job")},
             },
         ),
         migrations.AddField(
-            model_name='job',
-            name='favorite',
-            field=models.ManyToManyField(through='jobs.JobFavorite', to=settings.AUTH_USER_MODEL),
+            model_name="job",
+            name="favorite",
+            field=models.ManyToManyField(
+                through="jobs.JobFavorite", to=settings.AUTH_USER_MODEL
+            ),
         ),
     ]
