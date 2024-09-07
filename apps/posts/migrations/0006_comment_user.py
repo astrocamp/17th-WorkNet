f64113f99b453a13e89cf4d93a5b937a5a126388
ellen <wding975@gmail.com>
2024-09-07 16:34:37 +0800

    style: comment add style

--- apps/posts/migrations/0006_comment_user.py
+++ apps/posts/migrations/0006_comment_user.py
@@ -8,14 +8,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('posts', '0005_post_dislike_cnt_post_like_cnt_alter_post_user_and_more'),
+        ("posts", "0005_post_dislike_cnt_post_like_cnt_alter_post_user_and_more"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='comment',
-            name='user',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL),
+            model_name="comment",
+            name="user",
+            field=models.ForeignKey(
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                to=settings.AUTH_USER_MODEL,
+            ),
         ),
     ]
