89e1af017de2330dba29327f0d69f393624537be
Og <breezenest0912@gmail.com>
2024-09-07 11:00:52 +0800

    refactor: check format

--- apps/companies/views.py
+++ apps/companies/views.py
@@ -14,14 +14,17 @@ def index(request):
     companies = Company.objects.order_by("-id")
     return render(request, "companies/index.html", {"companies": companies})
 
+
 def new(request):
     form = CompanyForm()
     return render(request, "companies/new.html", {"form": form})
+
 
 def edit(request, id):
     company = get_object_or_404(Company, id=id)
     form = CompanyForm(instance=company)
     return render(request, "companies/edit.html", {"form": form, "company": company})
+
 
 def show(request, id):
     if request.method == "POST":
@@ -37,6 +40,7 @@ def show(request, id):
 
     company = get_object_or_404(Company, id=id)
     return render(request, "companies/show.html", {"company": company})
+
 
 @require_POST
 def delete(request, id):
