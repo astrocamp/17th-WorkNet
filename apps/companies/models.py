70b007a2e58d12868462faf4216b4437fc77eda1
Og <breezenest0912@gmail.com>
2024-09-07 10:58:58 +0800

    refactor: modify the 9/6 codereview companies section

--- apps/companies/models.py
+++ apps/companies/models.py
@@ -8,16 +8,13 @@ class Company(SoftDeletetable, models.Model):
     tel = models.CharField(max_length=15)
     url = models.URLField()
     address = models.CharField(max_length=300)
-    describe = models.TextField()
-    total_headcount = models.IntegerField()
+    description = models.TextField()
+    employees = models.IntegerField()
     name = models.CharField(max_length=200)
     email = models.EmailField()
-    owner_tel = models.CharField(max_length=15)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(default=None, null=True)
-
-    average_score = models.DecimalField(max_digits=5, decimal_places=1, default=0.0)
 
     objects = SoftDeleteManager()
 
