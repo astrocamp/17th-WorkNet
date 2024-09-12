8cf9415a3553924156d2d583861b28fd7ff66ca5
ellen <wding975@gmail.com>
2024-09-12 20:38:01 +0800

    fix: fix navbar & footer job button

--- apps/companies/migrations/0006_alter_company_user.py
+++ apps/companies/migrations/0006_alter_company_user.py
@@ -8,14 +8,19 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('companies', '0005_companyfavorite_company_favorite'),
+        ("companies", "0005_companyfavorite_company_favorite"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='company',
-            name='user',
-            field=models.OneToOneField(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='company', to=settings.AUTH_USER_MODEL),
+            model_name="company",
+            name="user",
+            field=models.OneToOneField(
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="company",
+                to=settings.AUTH_USER_MODEL,
+            ),
         ),
     ]
