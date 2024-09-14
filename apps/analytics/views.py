230cbf9ed7f8ce78a182eeeebfee420d3f4badaa
Ma <z2715674@gmail.com>
2024-09-14 12:46:21 +0800

    refactor: Remove redundant code

--- apps/analytics/views.py
+++ apps/analytics/views.py
@@ -13,17 +13,8 @@ def index(request):
         skill_list = job_skills.split(",")
         skill_counter.update(skill_list)
 
-    skill_counts_json = json.dumps(dict(skill_counter))  # 將 Counter 轉換為 JSON 字符串
+    skill_counts_json = json.dumps(dict(skill_counter))
 
     return render(
         request, "analytics/index.html", {"skill_counts_json": skill_counts_json}
     )
-
-
-# job_counts = list(Job.objects.values("company__name").annotate(count=Count("id")))
-
-# # 將數據轉換為 JSON 格式
-# job_counts_json = json.dumps(job_counts, cls=DjangoJSONEncoder)
-
-# # 將 JSON 傳遞給模板
-# return render(request, "analytics/index.html", {"job_counts_json": job_counts_json})
