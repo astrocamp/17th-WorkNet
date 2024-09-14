a37ce1c13412e8affb5234a1c60b6d05c90385e9
ellen <wding975@gmail.com>
2024-09-14 22:25:36 +0800

    test: test

--- apps/users/rule_required.py
+++ apps/users/rule_required.py
@@ -1,15 +1,21 @@
 from functools import wraps
 from django.http import HttpResponseForbidden
 import rules
+from .rules import user_can_view_info
+
+rules.add_rule("user_can_view_info", user_can_view_info)
+
 
 def rule_required(rule_name):
     def decorator(view_func):
         @wraps(view_func)
         def _wrapped_view(request, *args, **kwargs):
             user = request.user
-            target_user_id = kwargs.get('id')
+            target_user_id = kwargs.get("id")
             if not rules.test_rule(rule_name, user, target_user_id):
                 return HttpResponseForbidden("您沒有權限觀看此頁面")
             return view_func(request, *args, **kwargs)
+
         return _wrapped_view
+
     return decorator
--- apps/users/rules.py
+++ apps/users/rules.py
@@ -1,7 +1,6 @@
 import rules
 
+
 @rules.predicate
 def user_can_view_info(user, target_user_id):
     return user.is_authenticated and user.id == target_user_id
-
-rules.add_rule("user_can_view_info", user_can_view_info)
