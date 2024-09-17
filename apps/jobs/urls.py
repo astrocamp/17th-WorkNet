54fd4a9a011160afddf7dd541bb8bf0e5f93a2e3
Kk <iankk0806@gmail.com>
2024-09-18 01:04:35 +0800

    refactor: change apply_jobs & submit_jobs' urls

--- apps/jobs/urls.py
+++ apps/jobs/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 
+from apps.users import views as userviews
+
 from . import views
 
 app_name = "jobs"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:id>", views.show, name="show"),
     path("<int:id>/edit", views.edit, name="edit"),
     path("<int:id>/delete", views.delete, name="delete"),
+    path("<int:id>/apply", userviews.apply_jobs, name="apply_jobs"),
+    path("<int:id>submit", userviews.submit_jobs, name="submit_jobs"),
+    path("<int:id>", views.show, name="show"),
 ]
