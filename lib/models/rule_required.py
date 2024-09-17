cb884d2881a42dfb3ed5f296f1c2bba8837d81e9
Kk <iankk0806@gmail.com>
2024-09-17 17:11:23 +0800

    refactor: userinfo's page remove <int:id>

--- lib/models/rule_required.py
+++ lib/models/rule_required.py
@@ -9,8 +9,7 @@ def rule_required(rule_name):
         @wraps(view_func)
         def _wrapped_view(request, *args, **kwargs):
             user = request.user
-            target_user_id = kwargs.get("id")
-            if not rules.test_rule(rule_name, user, target_user_id):
+            if not rules.test_rule(rule_name, user):
                 return HttpResponseForbidden("您沒有權限觀看此頁面")
             return view_func(request, *args, **kwargs)
 
