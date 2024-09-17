64f9018e6063cda12db59b0f3ea3b72d57cfda1d
Kk <iankk0806@gmail.com>
2024-09-17 15:09:44 +0800

    refactor: rebase dev

--- config/settings.py
+++ config/settings.py
@@ -169,11 +169,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    
 ]
 
 LOGIN_URL = reverse_lazy("users:sign_in")
