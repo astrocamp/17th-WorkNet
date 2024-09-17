0237b74b8995b937d836c7154ae0a9f4d72d29e2
ellen <wding975@gmail.com>
2024-09-18 00:25:39 +0800

    fix: fix job analytics

--- apps/analytics/views.py
+++ apps/analytics/views.py
@@ -1,15 +1,19 @@
 import json
 from collections import Counter
 
+from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import render
+from taggit.models import TaggedItem
 
 from apps.jobs.models import Job
 
 
 def index(request):
-    tags = Job.tags.through.objects.values_list("tag__name", flat=True)
-    skill_counter = Counter(tags)
+    content_type = ContentType.objects.get_for_model(Job)
+    job_tag = TaggedItem.objects.filter(content_type=content_type)
 
+    tags = job_tag.values_list("tag__name", flat=True)
+    skill_counter = Counter(tags)
     skill_counts_json = json.dumps(dict(skill_counter))
 
     return render(
