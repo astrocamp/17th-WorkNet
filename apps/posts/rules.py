a53c34bbb88f3b28b6cdb364e36dcd69579cbe59
Og <breezenest0912@gmail.com>
2024-09-18 12:33:31 +0800

    refactor: missing,added modify button permissions

--- apps/posts/rules.py
+++ apps/posts/rules.py
@@ -1,9 +1,12 @@
 import rules
 from django.shortcuts import get_object_or_404
+
 from .models import Post
+
 
 @rules.predicate
 def can_edit_post(user, post):
     return user == post.user
 
-rules.add_rule('can_edit_post', can_edit_post)
+
+rules.add_rule("can_edit_post", can_edit_post)
