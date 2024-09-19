b9307dcdd22270dcce6a04cb75205700b03a1ec9
ellen <wding975@gmail.com>
2024-09-19 19:31:17 +0800

    refactor: transform tag to uppercase

--- src/scripts/tagInput.js
+++ src/scripts/tagInput.js
@@ -3,7 +3,11 @@ import Tagify from "@yaireo/tagify";
 
 Alpine.data("tag_input", () => ({
   init(existingTags) {
-    let input = new Tagify(this.$refs.tagInput);
+    let input = new Tagify(this.$refs.tagInput, {
+      transformTag: (tagData) => {
+        tagData.value = tagData.value.toUpperCase();
+      },
+    });
     input.addTags(existingTags);
 
     input.on("change", (e) => {
