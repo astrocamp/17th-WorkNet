84477f359b25ac4cb0069c9ec5c523f7e263c47e
Ma <z2715674@gmail.com>
2024-09-19 23:42:41 +0800

    feat: add user popular chars

--- apps/analytics/views.py
+++ apps/analytics/views.py
@@ -7,17 +7,28 @@ from taggit.models import TaggedItem
 
 from apps.jobs.models import Job
 from lib.utils.models.decorators import login_redirect_next
+from apps.users.models import UserInfo
 
 
 @login_redirect_next
 def index(request):
-    content_type = ContentType.objects.get_for_model(Job)
-    job_tag = TaggedItem.objects.filter(content_type=content_type)
+    job_content_type = ContentType.objects.get_for_model(Job)
+    job_tag = TaggedItem.objects.filter(content_type=job_content_type)
+    job_tags = job_tag.values_list("tag__name", flat=True)
+    job_skill_counter = Counter(job_tags)
+    job_skill_counts_json = json.dumps(dict(job_skill_counter))
 
-    tags = job_tag.values_list("tag__name", flat=True)
-    skill_counter = Counter(tags)
-    skill_counts_json = json.dumps(dict(skill_counter))
+    user_content_type = ContentType.objects.get_for_model(UserInfo)
+    user_tag = TaggedItem.objects.filter(content_type=user_content_type)
+    user_tags = user_tag.values_list("tag__name", flat=True)
+    user_skill_counter = Counter(user_tags)
+    user_skill_counts_json = json.dumps(dict(user_skill_counter))
 
     return render(
-        request, "analytics/index.html", {"skill_counts_json": skill_counts_json}
+        request,
+        "analytics/index.html",
+        {
+            "job_skill_counts_json": job_skill_counts_json,
+            "user_skill_counts_json": user_skill_counts_json,
+        },
     )
