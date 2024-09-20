3345f777454f584f07e8d3e5cb350e59f65c575a
Og <breezenest0912@gmail.com>
2024-09-20 12:14:06 +0800

    feat: add index RWD code

--- src/scripts/fontawesome.js
+++ src/scripts/fontawesome.js
@@ -4,21 +4,23 @@ import {
   faThumbsDown as fasThumbsDown,
   faThumbsUp as fasThumbsUp,
   faBell as fasBell,
+  faHeart as fasHeart,
+  faUser as fasUser,
+  faCaretDown as fasCaretDown,
+  faStar as fasStar,
+  faLocationDot as fasLocationDot,
+  faMagnifyingGlass as fasMagnifyingGlass,
+  faSackDollar as fasSackDollar,
 } from "@fortawesome/free-solid-svg-icons";
 import {
   faThumbsDown as farThumbsDown,
   faThumbsUp as farThumbsUp,
   faBell as farBell,
+  faHeart as farHeart,
 } from "@fortawesome/free-regular-svg-icons";
 
-library.add(
-  fasThumbsDown,
-  farThumbsDown,
-  fasThumbsUp,
-  farThumbsUp,
-  fasBell,
-  farBell
-);
+
+library.add(fasThumbsDown, farThumbsDown, fasThumbsUp, farThumbsUp,farBell,fasBell, fasHeart, farHeart, fasUser ,fasCaretDown ,fasStar , fasLocationDot,fasMagnifyingGlass,fasSackDollar);
 dom.i2svg();
 
 Alpine.data("reactions_post", () => ({
