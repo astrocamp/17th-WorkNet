901d4b1d1544ff9c9ab7b10d19cb1a11ac91ad73
Og <breezenest0912@gmail.com>
2024-09-20 12:14:06 +0800

    feat: add index RWD code

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,22 @@ module.exports = {
     extend: {
       backgroundImage: {
         'logo-backend': "url('/static/imgs/logo_backnend.png')",
+        'banner-img':"url('/static/imgs/banner.jpg')"
+      },
+      boxShadow: {
+        'custom-light': '0 3px 5px rgba(0, 0, 0, 0.05)',
       },
       colors: {
         'white-20':'rgba(255, 255, 255, 0.2)',
       },
       fontFamily: {
         'sans': ['Noto Sans TC', 'sans-serif'],
+      },
+      spacing: {
+        'calc-card': 'calc(50% - 20px)',
+      },
+      aspectRatio: {
+        '8/3': '8 / 3',
       },
       container: {
         screens: {
@@ -33,8 +43,10 @@ module.exports = {
       {
         corporate: {
           primary: "#00336b",
-          secondary: "#007bff",
+          secondary: "#449dd1",
           neutral: "#000000",
+          error:"#bb342f",
+          warning:"#ffe347",
         },
       },
     ],
