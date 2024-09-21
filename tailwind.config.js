b8c5720d526fc2c17d99ce47098d00c1a59042ac
Ma <z2715674@gmail.com>
2024-09-21 18:57:10 +0800

    fix: fix tailwind logo name

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,24 +9,24 @@ module.exports = {
     },
     extend: {
       backgroundImage: {
-        'logo-backend': "url('/static/imgs/logo_backnend.png')",
-        'banner-img':"url('/static/imgs/banner.jpg')"
+        "logo-backend": "url('/static/imgs/logo_backend.png')",
+        "banner-img": "url('/static/imgs/banner.jpg')",
       },
       boxShadow: {
-        'custom-light': '0 3px 5px rgba(0, 0, 0, 0.05)',
+        "custom-light": "0 3px 5px rgba(0, 0, 0, 0.05)",
       },
       colors: {
-        'white-20':'rgba(255, 255, 255, 0.2)',
+        "white-20": "rgba(255, 255, 255, 0.2)",
       },
       fontFamily: {
-        'sans': ['Noto Sans TC', 'sans-serif'],
+        sans: ["Noto Sans TC", "sans-serif"],
       },
       spacing: {
-        'calc-card': 'calc(50% - 20px)',
-        'calc-data': 'calc(50% - 10px)',
+        "calc-card": "calc(50% - 20px)",
+        "calc-data": "calc(50% - 10px)",
       },
       aspectRatio: {
-        '8/3': '8 / 3',
+        "8/3": "8 / 3",
       },
       container: {
         screens: {
@@ -46,8 +46,8 @@ module.exports = {
           primary: "#00336b",
           secondary: "#449dd1",
           neutral: "#000000",
-          error:"#bb342f",
-          warning:"#ffe347",
+          error: "#bb342f",
+          warning: "#ffe347",
         },
       },
     ],
