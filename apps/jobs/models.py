60dab910290d3b4b1828a26b3865b2b70ef1e49a
ellen <wding975@gmail.com>
2024-09-21 23:35:25 +0800

    feat: home set data

--- apps/jobs/models.py
+++ apps/jobs/models.py
@@ -5,32 +5,11 @@ from taggit.managers import TaggableManager
 from apps.companies.models import Company
 from apps.resumes.models import Resume
 from lib.models.soft_delete import SoftDeleteManager, SoftDeletetable
+from lib.utils.models.defined import LOCATION_CHOICES
 
 
 class Job(SoftDeletetable, models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="jobs")
-    LOCATION_CHOICES = [
-        ("Keelung", "基隆"),
-        ("Taipei", "台北"),
-        ("New Taipei", "新北"),
-        ("Taoyuan", "桃園"),
-        ("Hsinchu", "新竹"),
-        ("Miaoli", "苗栗"),
-        ("Taichung", "台中"),
-        ("Changhua", "彰化"),
-        ("Nantou", "南投"),
-        ("Yunlin", "雲林"),
-        ("Chiayi", "嘉義"),
-        ("Tainan", "台南"),
-        ("Kaohsiung", "高雄"),
-        ("Pingtung", "屏東"),
-        ("Taitung", "台東"),
-        ("Hualien", "花蓮"),
-        ("Yilan", "宜蘭"),
-        ("Penghu", "澎湖"),
-        ("Kinmen", "金門"),
-        ("Lienchiang", "連江"),
-    ]
     title = models.CharField(max_length=100, null=False, blank=False)
     description = models.TextField()
     location = models.CharField(max_length=100, choices=LOCATION_CHOICES)
