cdbf0ac42e5cae69135c4ed4236258f4774f3d4b
Kait <qw.kaitlyn@gmail.com>
2024-09-21 18:44:45 +0800

    fix: Add linebreak for post and markdown

--- apps/posts/templatetags/markdown.py
+++ apps/posts/templatetags/markdown.py
@@ -12,4 +12,15 @@ register = template.Library()
 def convert_markdown(value):
     value = re.sub(r"==(.+?)==", r"<mark>\1</mark>", value)
     value = re.sub(r"~~(.+?)~~", r"<del>\1</del>", value)
-    return markdown.markdown(value, extensions=["markdown.extensions.fenced_code"])
+    html = markdown.markdown(value, extensions=["markdown.extensions.fenced_code"])
+
+    def replace_linebreaks_outside_code(match):
+        content = match.group(2)
+        return match.group(1) + content.replace("\n", "<br>\n")
+
+    html = re.sub(
+        r"(<(?!pre|code)[^>]+>)([^<]+)", replace_linebreaks_outside_code, html
+    )
+
+    html = html.replace("</p>", "")
+    return html
