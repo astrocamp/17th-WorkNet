d0b99a2a46e1ecbd21b4cf723aed8865646a64a2
Gujiahan <gujiahan817@gmail.com>
2024-09-22 19:16:55 +0800

    feat: integrate Google Maps API
    BREAKING CHANGE:
    
    style: styles
    
    BREAKING CHANGE:

---
+++ static/scripts/map.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const initializeMap = () => {
+        const mapElement = document.getElementById('map');
+        const coord_x = parseFloat(mapElement.getAttribute('data-latitude'));
+        const coord_y = parseFloat(mapElement.getAttribute('data-longitude'));
+        const company_title = mapElement.getAttribute('data-company-title');
+
+        const map = new google.maps.Map(mapElement, {
+            zoom: 15,
+            center: { lat: coord_x, lng: coord_y },
+            mapId:'ea72dbee4f91d537'
+        });
+
+        const marker = new google.maps.marker.AdvancedMarkerElement({
+            position: { lat: coord_x, lng: coord_y },
+            map: map,
+            title: company_title
+        });
+
+        const infoWindow = new google.maps.InfoWindow({
+            content: `<h3>${company_title}</h3>`    
+        });
+
+        marker.addListener('click', () => {
+            infoWindow.open(map, marker);
+        });
+    };
+
+    // 檢查 google 是否定義
+    if (typeof google === 'object' && typeof google.maps === 'object') {
+        initializeMap();
+    } else {
+        window.addEventListener('load', initializeMap);
+    }
+});
