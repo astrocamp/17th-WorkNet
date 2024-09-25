7f2b91c6ca15e7eb4cba9e75f62592eed8f36956
Ma <z2715674@gmail.com>
2024-09-25 15:24:35 +0800

    style: add home programs tags hover

--- lib/utils/models/defined.py
+++ lib/utils/models/defined.py
@@ -24,16 +24,17 @@ LOCATION_CHOICES = [
     ("Lienchiang", "連江"),
 ]
 
+
 def fetch_coordinates(address):
     api_key = settings.GOOGLE_MAPS_API_KEY
     base_url = "https://maps.googleapis.com/maps/api/geocode/json"
-    params = {'address': address, 'key': api_key}
+    params = {"address": address, "key": api_key}
 
     response = requests.get(base_url, params=params)
 
     if response.status_code == 200:
         data = response.json()
-        if data ['results']:
-            location = data['results'][0]['geometry']['location']
-            return location['lat'], location['lng']
+        if data["results"]:
+            location = data["results"][0]["geometry"]["location"]
+            return location["lat"], location["lng"]
         return None, None
