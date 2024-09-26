ebf6c1c976645293c15fc7c57e4f60401369d15d
Ma <z2715674@gmail.com>
2024-09-26 20:04:31 +0800

    fix: fix F12 chart error

--- config/settings.py
+++ config/settings.py
@@ -187,9 +187,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 LOGIN_URL = reverse_lazy("users:sign_in")
 
