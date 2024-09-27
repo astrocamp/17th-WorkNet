e8757e99f9ed58be4d0806b824b754de44bc5333
Kait <qw.kaitlyn@gmail.com>
2024-09-27 15:26:58 +0800

    fix: Adjust resume dropdown filename
    fix: fix google maps error
    
    BREAKING CHANGE:

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,9 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     const initializeMap = () => {
         const mapElement = document.getElementById('map');
+        const errorElement = document.getElementById('map-error');
         const coord_x = parseFloat(mapElement.getAttribute('data-latitude'));
         const coord_y = parseFloat(mapElement.getAttribute('data-longitude'));
         const company_title = mapElement.getAttribute('data-company-title');
+        
+        if (isNaN(coord_x) || isNaN(coord_y)) {
+            mapElement.style.display = 'none';
+            errorElement.style.display = 'block';
+            return;
+        }
+
 
         const map = new google.maps.Map(mapElement, {
             zoom: 15,
